c5c3b6fc91d9562be2365d666f040535859c150e
raji <kingraj1344@gmail.com>
2024-03-02 15:49:17 +0100

    Update CORS configuration in app.js

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ const corsOptions = {
   origin: ['http://localhost:5000', 'http://sweetsensei.com'] // Whitelist the domains you want to allow
 };
 
-app.use(cors(corsOptions));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // request
 app.use(express.json());
 app.get("/", (req, res) => {
