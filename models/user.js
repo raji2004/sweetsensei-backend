d2381feab497411d710199fb97c2392197d027d8
raji <kingraj1344@gmail.com>
2024-03-14 07:50:54 +0100

    Add address field to UserSchema

--- models/user.js
+++ models/user.js
@@ -14,6 +14,9 @@ const UserSchema = new mongoose.Schema({
       type: String,
       required: true,
     },
+    address:{
+      type: String,
+    },
     date: {
       type: Date,
       default: Date.now,
