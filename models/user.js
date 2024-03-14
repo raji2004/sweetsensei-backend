574c87d5f4cd57d2737efccef9e36cc083b984b5
raji <kingraj1344@gmail.com>
2024-03-14 09:42:32 +0100

    Add additional fields to User model

--- models/user.js
+++ models/user.js
@@ -2,25 +2,64 @@ const { Timestamp } = require("mongodb");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    address:{
-      type: String,
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-  });
-  
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNo: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+
+  address: {
+    type: [{
+      pinCode: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      town: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      }
+    }],
+    default: [],
+  },
+  defaultAdress:{
+    type:Number,
+    default:0
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 module.exports = mongoose.model("User", UserSchema);
