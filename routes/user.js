a179a223f3e5f7190bedc3734cc87c21082f52bb
raji <kingraj1344@gmail.com>
2024-03-14 08:09:34 +0100

    Add new routes for user profile editing, profile retrieval, email authentication, and password reset

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,14 @@ router.route('/login')
     .post(user.login)
 router.route('/register')
     .post(user.register)
+router.route('/editprofile')
+    .post(user.editprofile)
+router.route('/getprofile')
+    .post(user.getprofile)
+router.route('/emailAuth')
+    .post(user.emailAuth)
+router.route('/resetPassword')
+    .post(user.resetPassword)
 
 
 module.exports = router;
